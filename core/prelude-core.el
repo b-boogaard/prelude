f3ed9494059d7c7ec81c8a3c3d5cb81690a1fcc9
Brian van de Boogaard (bv031773) <Brian.vandeBoogaard@Cerner.com>
2014-09-18 09:26:22 -0500

    Change github url to github.com

--- core/prelude-core.el
+++ core/prelude-core.el
@@ -85,7 +85,7 @@ PROMPT sets the `read-string prompt."
 
 (prelude-install-search-engine "google"     "http://www.google.com/search?q="              "Google: ")
 (prelude-install-search-engine "youtube"    "http://www.youtube.com/results?search_query=" "Search YouTube: ")
-(prelude-install-search-engine "github"     "https://github.cerner.com/search?q="                 "Search GitHub: ")
+(prelude-install-search-engine "github"     "https://github.com/search?q="                 "Search GitHub: ")
 (prelude-install-search-engine "duckduckgo" "https://duckduckgo.com/?t=lm&q="              "Search DuckDuckGo: ")
 
 (defun prelude-indent-rigidly-and-copy-to-clipboard (begin end arg)
