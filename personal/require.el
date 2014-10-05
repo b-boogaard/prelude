3f9963decf7613a70d7c99a610bbb08880582182
Brian van de Boogaard (bv031773) <Brian.vandeBoogaard@Cerner.com>
2014-10-05 16:03:41 -0500

    Remove standard solarized-theme from require.el

--- personal/require.el
+++ personal/require.el
@@ -1,5 +1,4 @@
 (prelude-require-packages '(use-package
-                             solarized-theme
                              js2-mode
                              multi-term
                              auto-complete))
